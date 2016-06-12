6e43d1e9977e9f319d98b9c34bd8dec49958c287
Yuri Kaspervich <ykas.gg@gmail.com>
2016-06-13 00:59:50 +0300

    Basic UI + Rating control

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,102 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Yuri Kasperovich on 6/11/16.
+//  Copyright © 2016 Yuri Kasperovich. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+    // MARK: Properties
+    
+    var rating = 0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    var ratingButtons = [UIButton]()
+    
+    let spacing = 5
+    let starCount = 5
+    
+    // MARK: Initialization
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        createButtons()
+    }
+    
+    override func intrinsicContentSize() -> CGSize {
+        return summarizeButtonSizes()
+    }
+
+    override func layoutSubviews() {
+        layoutButtons()
+        updateButtonSelectionStates()
+    }
+    
+    // MARK: Actions
+    
+    func ratingButtonTapped(button: UIButton) {
+        assignRating(button)
+        updateButtonSelectionStates()
+    }
+    
+    private
+    
+    func createButtons() {
+        for _ in 0..<starCount {
+            createButton()
+        }
+    }
+    
+    func createButton(emptyStarImage: UIImage, filledStarImage: UIImage) {
+        let button = UIButton()
+        
+        let emptyStarImage  = UIImage(named: "emptyStar")!
+        let filledStarImage = UIImage(named: "filledStar")!
+
+        button.setImage(emptyStarImage, forState: .Normal)
+        button.setImage(filledStarImage, forState: .Selected)
+        button.setImage(filledStarImage, forState: [.Highlighted, .Selected])
+        
+        button.adjustsImageWhenHighlighted = false
+        
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+        
+        ratingButtons += [button]
+        
+        addSubview(button)
+    }
+    
+    func summarizeButtonSizes() -> CGSize {
+        let buttonSize = Int(frame.size.height)
+        let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        
+        return CGSize(width: width, height: buttonSize)
+    }
+    
+    func layoutButtons() {
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
+            button.frame = buttonFrame
+        }
+    }
+    
+    func assignRating(button: UIButton) {
+        rating = ratingButtons.indexOf(button)! + 1
+    }
+    
+    func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerate() {
+            button.selected = index < rating
+        }
+    }
+}
--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var mealNameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mealNameTextField.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: Actions
+    
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        mealNameTextField.resignFirstResponder()
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .PhotoLibrary
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
     }
-
-
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        mealNameTextField.resignFirstResponder()
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        photoImageView.image = selectedImage
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
